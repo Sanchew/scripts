017f10ad32212dae13ebdfd18f47a991f355c440
lee <627051222@qq.com>
2017-08-01 11:36:34 +0800

    u

---
+++ shopifysupport.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Shopify Extends
+// @namespace    https://github.com/sanchew
+// @version      0.1
+// @description  Fix shopify UI and Features
+// @include      /^https?://[^\/]*?cemarose[^\/]*?\/.*?orders.*/
+// @author       sanchew
+// @license      MIT
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+if (window.tap == window.self) {
+  $('.next-input--number').val(0)
+}
